9f704c89b9e3e982b196f95c725710b0d1288cca
joenatech7 <joenatech@beast.com>
2019-03-14 14:49:01 -0400

    Initialize database

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
+from django.db import models
 
 
 class UserManager(BaseUserManager):
